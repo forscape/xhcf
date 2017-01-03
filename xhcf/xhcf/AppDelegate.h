c5c72417ea9007f503a192e54fc4edf9d88cc5f7
forscape <forscape@163.com>
2017-01-03 15:09:57 +0800

    初始化项目

---
+++ xhcf/xhcf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  xhcf
+//
+//  Created by 刘学 on 17/1/3.
+//  Copyright © 2017年 Ethan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ xhcf/xhcf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  xhcf
+//
+//  Created by 刘学 on 17/1/3.
+//  Copyright © 2017年 Ethan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
